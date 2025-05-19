ce566dee6c04b430f18b14623dcdbfebac082ad2
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-05-19 15:07:15 +1000

    Revert folder structure to default Vite settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <div>
-      <h1>Web Based Messaging App</h1>
-    </div>
-
+      <div>
+        <h1>Web Based Messaging App</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
