e9a5a4c1d91931d5d63ebfd14c6064856258ec69
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-05-22 16:07:34 +1000

    Update API points and controller functions

--- backend/src/controllers/mainController.js
+++ backend/src/controllers/mainController.js
@@ -1,4 +1,4 @@
-async function homePage(req, res, next) {
+async function apiStatus(req, res, next) {
   try {
     res.json({ status: "Backend Systems running ok" });
   } catch (error) {
@@ -6,4 +6,51 @@ async function homePage(req, res, next) {
   }
 }
 
-export { homePage };
+async function getAllContacts(req, res, next) {
+  try {
+    res.json({ status: "Get all contacts" });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getContactMessages(req, res, next) {
+  try {
+    res.json({ status: "Get logged in users messages" });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function sendMessageToContact(req, res, next) {
+  try {
+    res.json({ status: "Send messages to other user" });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function loadLoggedUserProfile(req, res, next) {
+  try {
+    res.json({ status: "Load logged in users profile" });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function updateLoggedUserProfile(req, res, next) {
+  try {
+    res.json({ status: "Load logged in users profile" });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export {
+  apiStatus,
+  getAllContacts,
+  getContactMessages,
+  sendMessageToContact,
+  loadLoggedUserProfile,
+  updateLoggedUserProfile,
+};
--- backend/src/routes/mainRoutes.js
+++ backend/src/routes/mainRoutes.js
@@ -1,19 +1,29 @@
 import { Router } from "express";
 
-import { homePage } from "../controllers/mainController.js";
+import {
+  apiStatus,
+  getAllContacts,
+  getContactMessages,
+  sendMessageToContact,
+  loadLoggedUserProfile,
+  updateLoggedUserProfile,
+} from "../controllers/mainController.js";
 
 const mainRouter = Router();
 
-mainRouter.get("/", homePage); // End Point to show the running status of backend systems
+mainRouter.get("/messaging-api/v1/status", apiStatus); // End Point to show the running status of backend systems
 
-mainRouter.get("/getallcontacts"); // Authenticated End Point to GET all the contacts and messages related current logged in user
+mainRouter.get("/messaging-api/v1/contacts", getAllContacts); // Authenticated End Point to GET all the contacts and messages related current logged in user
 
-mainRouter.get("/getcontactmessages"); // Authenticated End Point to GET messages sent and received by the logged in user
+mainRouter.get(
+  "/messaging-api/v1/contacts/:loggedInUserID/messages",
+  getContactMessages
+); // Authenticated End Point to GET messages sent and received by the logged in user
 
-mainRouter.put("/putmessagetocontact"); // Authenticated End Point to send message to selected contact
+mainRouter.post("/messaging-api/v1/messagetocontact", sendMessageToContact); // Authenticated End Point to send message to selected contact
 
-mainRouter.get("/getprofile"); // Authenticated End Point to get logged in users profile
+mainRouter.get("/messaging-api/v1/profile", loadLoggedUserProfile); // Authenticated End Point to get logged in users profile
 
-mainRouter.put("/getprofile"); // Authenticated End Point to update logged in users profile
+mainRouter.put("/messaging-api/v1/profile", updateLoggedUserProfile); // Authenticated End Point to update logged in users profile
 
 export default mainRouter;
