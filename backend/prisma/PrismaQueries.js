f4f7afde87f96cea6654fe84f815a7b1d54e8939
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-05-26 15:37:57 +1000

    Add PrismaQueries file and first query

---
+++ backend/prisma/PrismaQueries.js
@@ -0,0 +1,19 @@
+import { PrismaClient } from "../generated/prisma";
+
+import bcrypt from "bcryptjs";
+
+const prismaQuery = new PrismaClient();
+
+async function userMessages(userID) {
+  try {
+    const userMessages = await prismaQuery.message.findUnique({
+      where: { id: userID },
+    });
+    console.log(userMessages);
+    return userMessages;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export { userMessages };
