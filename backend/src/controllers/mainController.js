6c4c13b094764271bc4ed4fc3eacc99fa0022f29
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-05-27 10:34:23 +1000

    Enabled sending endpoint names in status route

--- backend/src/controllers/mainController.js
+++ backend/src/controllers/mainController.js
@@ -9,7 +9,17 @@ import {
 
 async function apiStatus(req, res, next) {
   try {
-    res.json({ status: "Backend Systems running ok" });
+    res.json({
+      status: "Backend Systems running ok",
+      endpoints: [
+        "/messaging-api/v1/status : GET End Point to show the running status of backend systems",
+        "/messaging-api/v1/contacts : GET Authenticated End Point to GET all the contacts and messages related current logged in user",
+        "/messaging-api/v1/contacts/:loggedInUserID/messages : GET Authenticated End Point to GET messages sent and received by the logged in user",
+        "/messaging-api/v1/messagetocontact : POST Authenticated End Point to send message to selected contact",
+        "/messaging-api/v1/profile : GET Authenticated End Point to get logged in users profile",
+        "/messaging-api/v1/profile : PUT Authenticated End Point to update logged in users profile",
+      ],
+    });
   } catch (error) {
     console.error(error);
   }
